b68e91b47743c950c4f179b86d8fc5def508ed4d
Obe Ajilima <ajilimaobe@gmail.com>
2024-07-08 16:32:10 +0100

    Added event listeners and conditionals for handling decimals, and rounded long decimal results
    This ensures that decimal input is handled correctly and that results are rounded to three decimal places for better readability and accuracy.

--- script.js
+++ script.js
@@ -35,49 +35,84 @@ function operate(operator, num1, num2) {
 
 let num1 = "";        
 let operator = "";   
-let num2 = "";        
+let num2 = "";       
 
 const calcScreen = document.querySelector(".calc-screen"); 
 const numbers = document.querySelectorAll(".number");
 const opSigns = document.querySelectorAll(".operator");
 const equalTo = document.querySelector(".equal-sign");
+const clear = document.querySelector(".all-clear");
+const decimal = document.querySelector(".decimal");
 
 const updateScreen = (value, type) => {
 
   if (type === "number") {
+
     if (!operator) {
       num1 += value;
-      calcScreen.value = num1; 
+      calcScreen.value = num1;
     }
+
     else {
       num2 += value;
+
       calcScreen.value = num1 + " " + operator + " " + num2;
     }
-  } 
-
+  }
+  
   else if (type === "operator") {
+
     if (num1 !== "") {
       operator = value; 
+
       calcScreen.value = num1 + " " + operator + " ";
     }
   } 
 
   else if (type === "equal") {
+
     if (num1 !== "" && num2 !== "" && operator !== "") { 
       const result = operate(operator, parseFloat(num1), parseFloat(num2));
-      calcScreen.value = result;
+      roundedResult = result.toFixed(3)
+      calcScreen.value = roundedResult;
+      num1 = roundedResult.toString(); 
+      num2 = ""; 
+      operator = ""; 
     } 
 
     else {
       calcScreen.value = "Incomplete input";
     }
   }
-};
+  
+  else if (type === "clear") {
+    calcScreen.value = "";
+    num1 = "";
+    num2 = "";
+    operator = "";
+  }
+
+  else if (type === "decimal") {
+    if (!operator) {
+
+      if (!num1.includes(".")) {
+        num1 += value;
+        calcScreen.value = num1;
+      }
+    } else {
+
+      if (!num2.includes(".")) {
+        num2 += value;
+        calcScreen.value = num1 + " " + operator + " " + num2;
+      } 
+    }
+  }
+}  
 
 numbers.forEach(number => { 
   number.addEventListener("click", () => {
     updateScreen(number.textContent, "number"); 
-  });
+  }); 
 });
 
 opSigns.forEach(opSign => {
@@ -88,4 +123,12 @@ opSigns.forEach(opSign => {
 
 equalTo.addEventListener("click", () => {
   updateScreen(null, "equal"); 
+});
+
+clear.addEventListener("click", () => {
+  updateScreen(null, "clear");
+});
+
+decimal.addEventListener("click", () => {
+  updateScreen(decimal.textContent, "decimal");
 });
